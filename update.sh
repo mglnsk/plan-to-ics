7b5093dead30d3fff2d4becdd51f858ee33b60e3
mglnsk <mgolanski@protonmail.com>
2024-09-21 11:53:16 +0200

    Remove unnecessary fork in update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit
 source ./venv/bin/activate
-
 mkdir -p calendars
 
-cat list.txt | while read line
+while read -r line
 do
     sleep $((RANDOM % 2))
-    #./create_ics.sh "https://wml.wat.edu.pl/wp-content/uploads/rozklady_zajec/2023_sem_lato/${line}.htm" "./calendars/${line}.ics"
     python3 ./create_ics.py --id "${line}" --output "./calendars/${line}.ics"
     sleep $((RANDOM % 8))
-done
+done < list.txt
 
 rsync -a calendars/ /var/www/wat/calendars/
